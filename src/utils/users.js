d9f29bbd5a9840797bd596d4871f8955523f3b51
cariejane <cariejaneestur@gmail.com>
2022-12-29 17:32:01 +0800

    edited

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,19 +1,55 @@
-const generateMessage = (text) => {
-    return {
-        text,
-        createdAt: new Date().getTime()
+const users = []
+
+const addUser = ({ id, username, room }) => {
+    //Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is already used!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
     }
 }
 
-const generateLocationMessage = (url) => {
-    return{
-        url,
-        createdAt: new Date().getTime()
-    }
-
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
 }
 
-module.exports = {
-    generateMessage,
-    generateLocationMessage
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports ={
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
 }
